13101da8bcc63f7e61a3385e37eb5c297549b2ce
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2019-05-07 19:30:49 -0400

    Using Interfaces to Describe Data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../product.service";
 import { Observable } from 'rxjs/Observable';
+import { Album } from '../album';
+
 
 @Component({
   selector: 'app-product-description',
@@ -8,7 +10,7 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo ;
+  albumInfo: Album ;
   constructor(private _productService : ProductService ) { }
 
   ngOnInit() {
