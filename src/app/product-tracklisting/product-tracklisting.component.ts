0eb9c8ab48e089084fea92d70a924e7225a47977
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2019-05-07 20:29:34 -0400

    imports

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Album } from '../album';
 import { ProductService } from '../product.service';
 
+
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
@@ -10,6 +11,7 @@ import { ProductService } from '../product.service';
 export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album ;
+  
   constructor(private _productService : ProductService ) { }
 
   ngOnInit() {
